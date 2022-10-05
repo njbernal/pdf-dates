f1edde45a5eb0ac971b0ba482ee5f868dd81640e
Nick Bernal <nickjbernal@gmail.com>
2022-10-05 16:29:55 -0700

    Created server and client environments and prepared dockers

---
+++ server/server.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/')
+@cross_origin()
+def index():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
