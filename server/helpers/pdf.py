ddbcf02daa9912c08e9c760833f4ae17c4b5d791
Nick Bernal <nickjbernal@gmail.com>
2022-10-05 18:43:53 -0700

    Created pdf.py and upload route in server

---
+++ server/helpers/pdf.py
@@ -0,0 +1,58 @@
+from dateutil.parser import parse, ParserError
+from dateutil.tz import tzutc
+from PyPDF2 import PdfReader
+from PyPDF2.errors import EmptyFileError
+
+
+class ReadError(Exception):
+    def __init__(self, filename):
+        self.message = "Unable to read PDF"
+
+
+def read_file(current_pdf):
+    try:
+        reader = PdfReader(current_pdf)
+    except EmptyFileError:
+        raise ReadError(current_pdf.filename)
+
+    pages = []
+    for page in reader.pages:
+        pages.append(page.extractText().split('\n'))
+    return pages
+
+
+def find_dates(contents):
+    file_results, count = [], 0
+    for page in contents:
+        page_results = []
+        for line in page:
+            try:
+                dates = parse(line, tzinfos={'FOX': tzutc()}, fuzzy=True)
+                page_results.append((dates.date(), line))
+            except ParserError:
+                pass
+
+        # Increase date count and append results to file_results
+        count += len(page_results)
+        file_results.append(page_results)
+
+    return {"count": count, "results": file_results, "error": False}
+
+
+def parsePDF(file):
+    # Default object structure
+    result = {
+        "count": 0,
+        "results": [],
+        "error": True
+    }
+
+    # Attempt to read PDF and extract dates
+    # ReadError will be raised if PDF is invalid.
+    try:
+        contents = read_file(file)
+        result = find_dates(contents)
+    except ReadError as error:
+        result["message"] = error.message
+
+    return result
--- server/server.py
+++ server/server.py
@@ -4,17 +4,32 @@
 """
 
 import os
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
+from helpers.pdf import parsePDF
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 @cross_origin()
 def index():
     return 'hello'
+
+
+@app.route('/api/upload', methods=['POST'])
+@cross_origin()
+def upload():
+    print(request.files)
+    if len(request.files) == 0:
+        return make_response("Record not found", 400)
+
+    result = {}
+    for file in request.files.getlist("source"):
+        result[file.filename] = parsePDF(file)
+
+    return result
 
 
 if __name__ == '__main__':
